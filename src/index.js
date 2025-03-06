4a4d4aa10bc9410e44ba84fb0b6cc954db52bd95
Anramilt <hiroko.anramilt@gmail.com>
2025-03-06 17:52:42 +0700

    add Article

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import SearchPage from "./SearchPage";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Article from './Article';
+import {articles} from './fixtures'  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +13,16 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+const article = articles[0]
+
+root.render(<Article article = {articles[0]} />, document.getElementById('container'))
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
