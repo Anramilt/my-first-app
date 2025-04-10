0cdd2d3cafdb11172186c0e6a4b18ebe8429f171
Anramilt <hiroko.anramilt@gmail.com>
2025-04-10 17:46:22 +0700

    Переделано2 масштабирование

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,67 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 import classes from "./LoginPage.module.css";
 
 import yandex from '../../Images/Regular_size/yandex.svg'
 import google from '../../Images/Regular_size/google.svg'
 import finalcleanlight from '../../Images/Regular_size/finalcleanlight.svg'
 
+
+/*http://localhost:8080/handle-register*/
+
+
 const LoginPage = () => {
   const navigate = useNavigate();
+  const [login, setLogin] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const HandleLogin = async (e) => {
+    try {
+      const formData = new URLSearchParams();
+      formData.append("login", login.trim());
+      formData.append("password", password.trim());
+
+      const response = await axios.post("http://localhost:8080/handle-login", formData, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+
+      console.log("Успешный вход:", response.data);
+      navigate("/");
+    } catch (error) {
+      console.error("Ошибка запроса:", error);
+    }
+  };
 
   return (
-    <div className={classes.mainConteiner}>
+    <div>
       <div className={classes.mainTheme}>
         <img src={finalcleanlight}></img>
       </div>
+      <div className={classes.mainContainer}>
 
-      <div className={classes.loginContainer}>
-        <div className={classes.title}>
-          <div className={classes.mobiTestText}>MobiTEST </div>
-          <div className={classes.systemEnterText}>Вход в систему </div>
-          <div className={classes.registrationText}>
-            <NavLink to="/register" className={navData => navData.isActive ? classes.activeLink : classes.item}>Зарегистрироваться</NavLink>
-          </div>
-          {/* <h2 className={classes.title}>Вход в систему</h2> */}
-        </div>
-
-        {/* <form action="http://localhost:8080/handle-login" method="POST"> */}
-        <div className={classes.inputBox}>
-          {/* <label htmlFor="login">Логин:</label><br /> */}
-          <input type="text" placeholder="Логин/почта" className={classes.input} required /><br /><br />
-        </div>
-
-        <div className={classes.inputBox}>
-          {/* <label htmlFor="password">Пароль:</label><br /> */}
-          <input type="password" placeholder="Пароль" className={classes.input} required /><br /><br />
-        </div>
-
-
-        <div className={classes.buttonСontainer}>
-          {/* <button type="button" className={classes.button} onClick={() => navigate(-1)}>Назад</button> */}
-          <button type="submit" className={classes.button}>Войти</button>
-        </div>
-        {/* </form> */}
-        <div className={classes.registrationButtonsWrapper}>
-          <div className={classes.iconButton}>
-            <div className={classes.yandexIcon}>
-              <div className={classes.yandexImgIcon}>
-                <img
-                  src={yandex}
-                  effect="blur"
-                />
-              </div>
+        <div className={classes.loginContainer}>
+          <div className={classes.title}>
+            <div className={classes.mobiTestText}>MobiTEST </div>
+            <div className={classes.systemEnterText}>Вход в систему </div>
+            <div className={classes.registrationText}>
+              <NavLink to="/register" className={navData => navData.isActive ? classes.activeLink : classes.item}>Зарегистрироваться</NavLink>
             </div>
           </div>
-          <div className={classes.iconButton}>
-            <div className={classes.googleIcon}>
-              <div className={classes.googleImgIcon}>
-                <img
-                  src={google}
-                  effect="blur"
-                />
-              </div>
 
+          {/* <form action="http://localhost:8080/handle-login" method="POST"> */}
+          <div className={classes.inputBox}>
+            <input 
+              type="text" 
+              placeholder="Логин/почта" 
+              className={classes.input} required 
+              value={login} 
+              onChange={(e) => setLogin(e.target.value)} 
+            />
+          </div>
+
+          <div className={classes.inputBox}>
+            <input 
+              type="password" 
+              placeholder="Пароль" 
+              className={classes.input} required 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+          </div>
+
+
+          <div className={classes.buttonСontainer}>
+            <button type="submit" className={classes.button} onClick={HandleLogin}>Войти</button>
+          </div>
+          {/* </form> */}
+          <div className={classes.registrationButtonsWrapper}>
+            <div className={classes.iconButton}>
+              <div className={classes.yandexIcon}>
+                <div className={classes.yandexImgIcon}>
+                  <img
+                    src={yandex}
+                    effect="blur"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={classes.iconButton}>
+              <div className={classes.googleIcon}>
+                <div className={classes.googleImgIcon}>
+                  <img
+                    src={google}
+                    effect="blur"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import classes from "./RegisterPage.module.css";
+import axios from "axios";
 
 import yandex from '../../Images/Regular_size/yandex.svg'
 import google from '../../Images/Regular_size/google.svg'
@@ -9,13 +10,40 @@ import finalcleanlight from '../../Images/Regular_size/finalcleanlight.svg'
 
 const RegisterPage = () => {
   const navigate = useNavigate();
+  const [login, setLogin] = useState("")
+  const [password, setPassword] = useState("")
+  const [key, setKey] = useState("")
+
+  const HandleRegister = async (e) => {
+    try {
+      const formData = new URLSearchParams();
+      formData.append("login", login.trim());
+      formData.append("password", password.trim());
+      formData.append("key", key.trim());
+
+      const response = await axios.post("http://localhost:8080/handle-register", formData, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+
+      console.log("Успешная регистрация аккаунта:", response.data);
+      navigate("/");
+    } catch (error) {
+      console.error("Ошибка запроса:", error);
+    }
+  };
+
+
+
 
   return (
-    <div className={classes.mainConteiner}>
+    <div>
       <div className={classes.mainTheme}>
         <img src={finalcleanlight}></img>
       </div>
-      <form action="http://localhost:8080/handle-register" method="POST">
+      <div className={classes.mainContainer}>
+
         <div className={classes.registerContainer}>
           <div className={classes.title}>
             <div className={classes.mobiTestText}>MobiTEST </div>
@@ -23,58 +51,70 @@ const RegisterPage = () => {
             <div className={classes.registrationText}>
               <NavLink to="/" className={navData => navData.isActive ? classes.activeLink : classes.item}>Войти в аккаунт</NavLink>
             </div>
-            {/* <h2 className={classes.title}>Вход в систему</h2> */}
           </div>
 
           {/* <form action="http://localhost:8080/handle-login" method="POST"> */}
           <div className={classes.inputBox}>
-            {/* <label htmlFor="login">Логин:</label><br /> */}
-            <input type="text" placeholder="Логин/почта" className={classes.input} required /><br /><br />
+            <input 
+              type="text" 
+              placeholder="Логин/почта" 
+              className={classes.input} required 
+              value={login} 
+              onChange={(e) => setLogin(e.target.value)}
+            />
           </div>
 
           <div className={classes.inputBox}>
-            {/* <label htmlFor="password">Пароль:</label><br /> */}
-            <input type="password" placeholder="Пароль" className={classes.input} required /><br /><br />
+            <input 
+              type="password" 
+              placeholder="Пароль" 
+              className={classes.input} required 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
 
           <div className={classes.inputBox}>
-            {/* <label htmlFor="password">Пароль:</label><br /> */}
-            <input type="key" placeholder="Ключ доступа" className={classes.input} required /><br /><br />
+            <input 
+              type="key" 
+              placeholder="Ключ доступа" 
+              className={classes.input} required 
+              value={key} 
+              onChange={(e) => setKey(e.target.value)}
+            />
           </div>
 
 
           <div className={classes.buttonСontainer}>
-            {/* <button type="button" className={classes.button} onClick={() => navigate(-1)}>Назад</button> */}
-            <button type="submit" className={classes.button}>Зарегистрироваться</button>
+            <button type="submit" className={classes.button} onClick={HandleRegister}>Зарегистрироваться</button>
           </div>
           {/* </form> */}
+          
           <div className={classes.registrationButtonsWrapper}>
-          <div className={classes.iconButton}>
-            <div className={classes.yandexIcon}>
-              <div className={classes.yandexImgIcon}>
-                <img
-                  src={yandex}
-                  effect="blur"
-                />
+            <div className={classes.iconButton}>
+              <div className={classes.yandexIcon}>
+                <div className={classes.yandexImgIcon}>
+                  <img
+                    src={yandex}
+                    effect="blur"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={classes.iconButton}>
+              <div className={classes.googleIcon}>
+                <div className={classes.googleImgIcon}>
+                  <img
+                    src={google}
+                    effect="blur"
+                  />
+                </div>
               </div>
             </div>
           </div>
-          <div className={classes.iconButton}>
-            <div className={classes.googleIcon}>
-              <div className={classes.googleImgIcon}>
-                <img
-                  src={google}
-                  effect="blur"
-                />
-              </div>
-
-            </div>
-          </div>
         </div>
-        </div>
-      </form>
+      </div>
     </div>
-
   );
 };
 
